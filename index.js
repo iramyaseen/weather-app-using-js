a48d9987fc7009df8be30538f6505e8e63068556
iramyaseen <75573544+iramyaseen@users.noreply.github.com>
2025-06-14 17:08:00 +0500

    refactor: update weather display logic and adjust location font size

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const api_key = "815d31dfc1204ba5ba995612251306";
 
-// const api_url = `https://api.openweathermap.org/data/2.5/weather?q=noorpur&appid=${api_key}`;
-
 async function getWeather(city = "sahiwal") {
   const api_url = `http://api.weatherapi.com/v1/current.json?key=${api_key}&q=${city}`;
   const response = await fetch(api_url);
@@ -11,32 +9,38 @@ async function getWeather(city = "sahiwal") {
     document.querySelector(".error-message").innerText = data.error.message;
     document.querySelector(".location").style.display = "none";
     document.querySelector(".temperature").style.display = "none";
-
     document.querySelector(".detail-info").style.display = "none";
+    document.querySelector(".detail-in").style.display = "none";
     document.querySelector(".weather-details").style.display = "none";
-
     document.querySelector(".weather-icon img").style.display = "none";
     return;
-  } else {
-    document.querySelector(".location").innerText = data.location.name;
-    document.querySelector(".temperature").innerText =
-      Math.round(data.current.temp_c) + "°C";
-
-    document.querySelector(".detail-info .value").innerText =
-      data.current.humidity + "%";
-    document.querySelector(".detail-in .value").innerText =
-      data.current.wind_kph + " km/h";
-
-    document.querySelector(".weather-icon img").src =
-      data.current.condition.icon;
   }
+
+  document.querySelector(".location").innerText = data.location.name;
+  document.querySelector(".temperature").innerText =
+    Math.round(data.current.temp_c) + "°C";
+  document.querySelector(".detail-info .value").innerText =
+    data.current.humidity + "%";
+  document.querySelector(".detail-in .value").innerText =
+    data.current.wind_kph + " km/h";
+  document.querySelector(".weather-icon img").src = data.current.condition.icon;
 }
 
 const searchButton = document.querySelector(".search-container button");
 searchButton.addEventListener("click", () => {
   const city = document.querySelector(".search-container input").value;
+
+  document.querySelector(".error-message").innerText = "";
+  document.querySelector(".location").style.display = "block";
+  document.querySelector(".temperature").style.display = "block";
+  document.querySelector(".detail-info").style.display = "block";
+  document.querySelector(".detail-in").style.display = "block";
+  document.querySelector(".weather-details").style.display = "flex";
+  document.querySelector(".weather-icon img").style.display = "block";
+
   if (city) {
     getWeather(city);
   }
 });
+
 getWeather();
